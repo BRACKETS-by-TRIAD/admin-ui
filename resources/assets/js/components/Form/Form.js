00bda2195c74520488881ed4f21ee2515df8475c
Pavol Perdík <palypster@gmail.com>
2017-06-29 20:04:16 +0200

    wip

--- resources/assets/js/components/Form/Form.js
+++ resources/assets/js/components/Form/Form.js
@@ -10,19 +10,33 @@ module.exports = {
         },
         'defaultLocale': {
             type: String,
+            default: function() {
+                return this.locales[0];
+            }
+        },
+        'sendEmptyLocales': {
+            type: Boolean,
+            default: function() {
+                return true;
+            }
         },
         'default': {
             type: Object,
             default: function() {
-                var o = {};
-
-                if (!!this.locales && this.locales.length > 0) {
-                    this.locales.map(x => o[x] = {});
-                }
-
-                return o;
+                return {};
             }
         },
+    },
+
+    created: function() {
+        if (!!this.locales && this.locales.length > 0) {
+            let form = this.form
+            this.locales.map(function(l) {
+                if (!_.has(form, l)) {
+                    _.set(form, l, {})
+                }
+            })
+        }
     },
 
     data: function() {
@@ -33,6 +47,12 @@ module.exports = {
         }
     },
 
+    computed: {
+        otherLocales: function() {
+            return this.locales.filter(x => x != this.defaultLocale);
+        },
+    },
+
     methods: {
         onSubmit() {
             return this.$validator.validateAll()
@@ -40,7 +60,13 @@ module.exports = {
                     if (!result) {
                         return false;
                     }
-                    axios.post(this.action, this.form)
+
+                    var data = this.form;
+                    if (!this.sendEmptyLocales) {
+                        data = _.omit(this.form, this.locales.filter(locale => _.isEmpty(this.form[locale])));
+                    }
+
+                    axios.post(this.action, data)
                         .then(response => this.onSuccess(response.data))
                         .catch(errors => this.onFail(errors.response.data))
                 });
@@ -53,7 +79,7 @@ module.exports = {
             Object.keys(errors).map(function(key) {
                 var splitted = key.split('.', 2);
                 if (splitted.length > 1) {
-                    bag.add(splitted[1], errors[key][0], null, splitted[0]);
+                    bag.add(splitted[0]+'_'+splitted[1], errors[key][0], null);
                 } else {
                     bag.add(key, errors[key][0]);
                 }
