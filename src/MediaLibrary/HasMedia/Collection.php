902be321440fee929eb0a9f713edc2f0eb7fb43a
Pavol Perdík <palypster@gmail.com>
2017-07-14 16:01:28 +0200

    wip

--- src/MediaLibrary/HasMedia/Collection.php
+++ src/MediaLibrary/HasMedia/Collection.php
@@ -5,10 +5,13 @@ namespace Brackets\Admin\MediaLibrary\HasMedia;
 class Collection  {
 
     protected $is_image = false;
+    protected $name;
+    protected $disk;
 
     public function __construct(string $name)
     {
         $this->name = $name;
+        $this->disk = config('medialibrary.default_public_disk', 'media');
     }
 
     public static function create(string $name)
@@ -16,14 +19,43 @@ class Collection  {
         return new static($name);
     }
 
+    // FIXME should it be here?
     public function title($title) {
         $this->title = $title;
         return $this;
     }
 
+    /**
+     * Set this collection contains an images. This allows the conversions functionality.
+     *
+     * @return $this
+     */
     public function image() {
         $this->is_image = true;
         return $this;
     }
+
+    /**
+     * Specify a disk where to store this collection
+     *
+     * @param $disk
+     * @return $this
+     */
+    public function disk($disk) {
+        $this->disk = $disk;
+        return $this;
+    }
+
+    /**
+     * Alias to setting default protected disk
+     *
+     * @return $this
+     */
+    public function protected() {
+        $this->disk = config('medialibrary.default_protected_disk');
+        return $this;
+    }
+
+    // TODO
 
 }
