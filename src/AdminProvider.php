a07a915a0ad44b22934ccc2e67d052297177f1e3
Marek Kratochvíla <kratochvila.marek@gmail.com>
2017-07-24 11:04:50 +0200

    backend presunuty do samostatnej package

--- src/AdminProvider.php
+++ src/AdminProvider.php
@@ -13,12 +13,6 @@ class AdminProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'brackets/admin');
-        $this->loadRoutesFrom(__DIR__.'/MediaLibrary/Http/routes.php'); //FIXME:: ako sa routes prefixuju s brackets/admin?
-        
-        $this->publishes([
-            __DIR__.'/MediaLibrary/config' => base_path('config')
-        ], 'config');
-
 
         $this->publishes([
             __DIR__.'/../install-stubs/resources/assets' => resource_path('assets')
@@ -38,9 +32,6 @@ class AdminProvider extends ServiceProvider
      */
     public function register()
     {
-        //FIXME: lepsie by bolo keby sa to dalo publishnut do filesystems
-        $this->mergeConfigFrom(
-            __DIR__.'/MediaLibrary/config/filesystems.php', 'filesystems.disks'
-        );
+
     }
 }
--- src/MediaLibrary/config/simpleweb-medialibrary.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'default_public_disk' => 'media',
-	'default_protected_disk' => 'media-protected'
-];
