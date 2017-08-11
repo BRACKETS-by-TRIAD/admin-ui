6ab37f3533d793d020d6bfd966a6e3a997eebc39
Pavol Perdík <palypster@gmail.com>
2017-08-11 23:49:46 +0200

    ..

--- install-stubs/resources/views/admin/layout/sidebar.blade.php
+++ install-stubs/resources/views/admin/layout/sidebar.blade.php
@@ -1,20 +1,20 @@
 <div class="sidebar">
     <nav class="sidebar-nav">
         <ul class="nav">
+            <li class="nav-title">Content</li>
             {{-- Do not delete me :) I'm used for auto-generation menu items --}}
 
-            <li class="nav-title">E-shop</li>
+            {{--<li class="nav-title">E-shop</li>--}}
 
-            <li class="nav-item"><a class="nav-link" href="#"><i class="icon-basket"></i> Orders</a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="icon-grid"></i> Products</a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="icon-people"></i> Customers</a></li>
+            {{--<li class="nav-item"><a class="nav-link" href="#"><i class="icon-basket"></i> Orders</a></li>--}}
+            {{--<li class="nav-item"><a class="nav-link" href="#"><i class="icon-grid"></i> Products</a></li>--}}
+            {{--<li class="nav-item"><a class="nav-link" href="#"><i class="icon-people"></i> Customers</a></li>--}}
 
             <li class="nav-title">Settings</li>
 
-            <li class="nav-item"><a class="nav-link" href="#"><i class="icon-user"></i> Admin access</a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="icon-location-pin"></i> Localization</a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="icon-settings"></i> Configuration</a></li>
-            </li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/user') }}"><i class="icon-user"></i> {{ __('Manage access') }}</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/translation') }}"><i class="icon-location-pin"></i> {{ __('Translations') }}</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/configuration') }}"><i class="icon-settings"></i> {{ __('Configuration') }}</a></li>
         </ul>
     </nav>
 </div>
