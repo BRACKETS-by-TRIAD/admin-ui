1e5b99278a5476d07bb469dd5dec6e7dc8f57d36
Pavol Perdík <palypster@gmail.com>
2017-08-28 14:15:33 +0200

    wip

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -15,6 +15,22 @@ return [
         'search' => "Search",
     ],
 
+    'index' => [
+        'no_items' => "Could not find any items",
+        'try_changing_items' => "Try changing the filters or add a new one",
+    ],
+
+    'forms' => [
+        'select_a_date' => 'Select a date',
+        'select_a_time' => 'Select a time',
+        'select_date_and_time' => 'Select date and time',
+        'choose_translation_to_edit' => 'Choose translation to edit:',
+        'manage_translations' => 'manage translations',
+        'more_can_be_managed' => '({{ otherLocales.length }} more can be managed)',
+        'currently_editing_translation' => 'Currently editing <strong>{{ this.defaultLocale.toUpperCase() }} (default)</strong> translation',
+        'hide' => 'hide',
+    ],
+
     'placeholder' => [
         'search' => "Search"
     ],
