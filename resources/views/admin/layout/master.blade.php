b5c9326c033644408b8f5b00dfba5b019f6383fa
Matej Minar <matej.minar@triad.sk>
2017-08-28 09:58:55 +0200

    welcome screen styles

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -6,11 +6,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-	{{-- TODO dat suffix do prekladaca --}}
+	{{-- TODO translatable suffix --}}
     <title>@yield('title', 'Simpleweb') - Simpleweb</title>
 
 	@include('brackets/admin::admin.partials.main-styles')
