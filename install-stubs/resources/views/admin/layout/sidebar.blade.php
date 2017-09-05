fa6835b6661d119343adf4b63b4eb19692fa6f75
Pavol Perdík <palypster@gmail.com>
2017-09-05 18:23:21 +0200

    wip

--- install-stubs/resources/views/admin/layout/sidebar.blade.php
+++ install-stubs/resources/views/admin/layout/sidebar.blade.php
@@ -5,15 +5,12 @@
             {{-- Do not delete me :) I'm used for auto-generation menu items --}}
 
             {{--<li class="nav-title">E-shop</li>--}}
-
             {{--<li class="nav-item"><a class="nav-link" href="#"><i class="icon-basket"></i> Orders</a></li>--}}
             {{--<li class="nav-item"><a class="nav-link" href="#"><i class="icon-grid"></i> Products</a></li>--}}
             {{--<li class="nav-item"><a class="nav-link" href="#"><i class="icon-people"></i> Customers</a></li>--}}
 
             <li class="nav-title">Settings</li>
-
-            <li class="nav-item"><a class="nav-link" href="{{ url('admin/user') }}"><i class="icon-user"></i> <span class="nav-link-text">{{ __('Manage access') }}</span></a></li>
-            <li class="nav-item"><a class="nav-link" href="{{ url('admin/translation') }}"><i class="icon-location-pin"></i> <span class="nav-link-text">{{ __('Translations') }}</span></a></li>
+            {{-- Do not delete me :) I'm also used for auto-generation menu items --}}
             {{--<li class="nav-item"><a class="nav-link" href="{{ url('admin/configuration') }}"><i class="icon-settings"></i> <span class="nav-link-text">{{ __('Configuration') }}</span></a></li>--}}
         </ul>
 
