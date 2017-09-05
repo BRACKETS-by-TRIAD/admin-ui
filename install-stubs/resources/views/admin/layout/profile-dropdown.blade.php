e157626c7d00b0845f88fb6bcfee85897505a371
Pavol Perdík <palypster@gmail.com>
2017-09-05 19:18:15 +0200

    wip

--- install-stubs/resources/views/admin/layout/profile-dropdown.blade.php
+++ install-stubs/resources/views/admin/layout/profile-dropdown.blade.php
@@ -1,7 +1,4 @@
 <div class="dropdown-menu dropdown-menu-right">
     <div class="dropdown-header text-center"><strong>Account</strong></div>
     {{-- Do not delete me :) I'm used for auto-generation menu items --}}
-{{--    <a href="{{ url('admin/profile') }}" class="dropdown-item"><i class="fa fa-user"></i> Profile</a>--}}
-{{--    <a href="{{ url('admin/password') }}" class="dropdown-item"><i class="fa fa-key"></i> Password</a>--}}
-    {{--<a href="{{ url('admin/logout') }}" class="dropdown-item"><i class="fa fa-lock"></i> Logout</a>--}}
 </div>
