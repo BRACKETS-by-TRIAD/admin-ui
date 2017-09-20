599046ad9c07b401d04fe9d128c398926125fa0b
Matej Minar <matej.minar@triad.sk>
2017-09-20 16:15:35 +0200

    sidebar icon

--- install-stubs/resources/views/admin/layout/sidebar.blade.php
+++ install-stubs/resources/views/admin/layout/sidebar.blade.php
@@ -16,6 +16,7 @@
 
         <div class="sidebar-collapse">
             <i class="fa fa-angle-double-left"></i>
+            <i class="fa fa-angle-double-right"></i>
         </div>
     </nav>
 </div>
