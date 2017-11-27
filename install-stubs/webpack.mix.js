c68a1f1d8d13df266e85bc8b4b5ac47ab35bdd00
Matej Minar <matej.minar@triad.sk>
2017-11-27 16:30:10 +0100

    edited webpack

--- install-stubs/webpack.mix.js
+++ install-stubs/webpack.mix.js
@@ -1,6 +1,3 @@
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
-
 mix.js(['resources/assets/admin/js/admin.js'], 'public/build/admin/js')
     .sass('resources/assets/admin/scss/app.scss', 'public/build/admin/css');
 
