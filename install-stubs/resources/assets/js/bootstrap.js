f78995229e8b9f6e3a160f05196c0b4c774ae25b
Oscar Mwanandimai <oscar.mwana@gmail.co.za>
2018-01-16 10:35:47 +0200

    added bootstrap and reduced lines of code for pulling dependencies

--- install-stubs/resources/assets/js/bootstrap.js
+++ install-stubs/resources/assets/js/bootstrap.js
@@ -1,14 +1,10 @@
-import axios from 'axios';
-import _ from 'lodash';
-import Vue from 'vue';
-import jQuery from 'jquery';
-import moment from 'moment';
-
-window.$ = window.jQuery = jQuery;
-window.Vue = Vue;
-window._ = _;
-window.axios = axios;
-window.moment = moment;
+window.$ = window.jQuery = require("jquery");
+window.axios = require("axios");
+window.Vue = require("vue");
+window._ = require("lodash");
+window.moment = require("moment");
+window.Popper = require("popper.js").default;
+require("bootstrap");
 
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
