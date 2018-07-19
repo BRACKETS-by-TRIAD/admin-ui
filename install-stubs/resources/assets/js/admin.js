4b39625ae5044ac2b1d1673236583b4bbd8cfe29
Matej Minar <mato7221@gmail.com>
2018-07-19 09:25:04 +0200

    Media wysiwyg (#17)
    * wysiwyg config, migration
    
    * copying migration and config during installation
    
    * wysiwyg media trait and model
    
    * media wysiwyg controller with route
    
    * connected routes
    
    * prefixed wysiwyg media upload route
    
    * removed doubled migration line
    
    * added validation in wysiwyg media upload controller
    
    * media ids validation in wysiwyg trait
    
    * initialization of new wysiwyg plugin

--- install-stubs/resources/assets/js/admin.js
+++ install-stubs/resources/assets/js/admin.js
@@ -1,6 +1,10 @@
+import './bootstrap';
+
 import 'vue-multiselect/dist/vue-multiselect.min.css';
 import flatPickr from 'vue-flatpickr-component';
 import VueQuillEditor from 'vue-quill-editor';
+import VueTrumbowyg from 'vue-trumbowyg';
+import 'trumbowyg/dist/ui/trumbowyg.css';
 import Notifications from 'vue-notification';
 import Multiselect from 'vue-multiselect';
 import VeeValidate from 'vee-validate';
@@ -10,7 +14,6 @@ import { Admin } from 'craftable';
 import VModal from 'vue-js-modal'
 import Vue from 'vue';
 
-import './bootstrap';
 import './app-components/bootstrap';
 import './index';
 
@@ -19,6 +22,7 @@ import 'craftable/coreui';
 Vue.component('multiselect', Multiselect);
 Vue.use(VeeValidate, {strict: true});
 Vue.component('datetime', flatPickr);
+Vue.component('wysiwyg', VueTrumbowyg);
 Vue.use(VModal, { dialog: true });
 Vue.use(VueQuillEditor);
 Vue.use(Notifications);
