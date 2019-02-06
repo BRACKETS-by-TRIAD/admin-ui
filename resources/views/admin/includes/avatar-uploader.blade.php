aa4dc65920ea9490f8cf381fd1be26c709db418e
Richard <riso.dominik@gmail.com>
2019-02-06 13:46:38 +0100

    avatar-uploader added, header support avatars

---
+++ resources/views/admin/includes/avatar-uploader.blade.php
@@ -0,0 +1,17 @@
+<media-upload
+        ref="{{ $mediaCollection->getName() }}_uploader"
+        :collection="'{{ $mediaCollection->getName() }}'"
+        :url="'{{ route('brackets/media::upload') }}'"
+        @if($mediaCollection->getMaxNumberOfFiles())
+        :max-number-of-files="{{ $mediaCollection->getMaxNumberOfFiles() }}"
+        @endif
+        @if($mediaCollection->getMaxFileSize())
+        :max-file-size-in-mb="{{ round(($mediaCollection->getMaxFileSize()/1024/1024), 2) }}"
+        @endif
+        @if($mediaCollection->getAcceptedFileTypes())
+        :accepted-file-types="'{{ implode($mediaCollection->getAcceptedFileTypes(), '') }}'"
+        @endif
+        @if(isset($media) && $media->count() > 0)
+        :uploaded-images="{{ $media->toJson() }}"
+        @endif
+></media-upload>
