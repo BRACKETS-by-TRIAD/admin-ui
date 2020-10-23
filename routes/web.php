3d2644f2223eca21b4eaeb519b59f92bb3665ac1
Richard <riso.dominik@gmail.com>
2020-10-23 10:53:10 +0200

    laravel-8 update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Brackets\AdminUI\Http\Controllers\WysiwygMediaUploadController;
+
 Route::middleware(['web', 'admin'])->group(function () {
     Route::namespace('Brackets\AdminUI\Http\Controllers')->group(function () {
-        Route::post('/admin/wysiwyg-media','WysiwygMediaUploadController@upload')->name('brackets/admin-ui::wysiwyg-upload');
+        Route::post('/admin/wysiwyg-media',  [WysiwygMediaUploadController::class, 'upload'])->name('brackets/admin-ui::wysiwyg-upload');
     });
 });
