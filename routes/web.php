020685f2c30ca038a66d58bee85e6529b871f9c6
Richard <riso.dominik@gmail.com>
2020-10-23 10:57:36 +0200

    code style fixed

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Brackets\AdminUI\Http\Controllers\WysiwygMediaUploadController;
 
 Route::middleware(['web', 'admin'])->group(function () {
     Route::namespace('Brackets\AdminUI\Http\Controllers')->group(function () {
-        Route::post('/admin/wysiwyg-media',  [WysiwygMediaUploadController::class, 'upload'])->name('brackets/admin-ui::wysiwyg-upload');
+        Route::post('/admin/wysiwyg-media', [WysiwygMediaUploadController::class, 'upload'])->name('brackets/admin-ui::wysiwyg-upload');
     });
 });
