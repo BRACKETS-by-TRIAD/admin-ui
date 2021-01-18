75b8f5374ca05c8fbd072482102d8f8e9e801e19
Stanislav Varga <stanislav.varga@brackets.sk>
2021-01-18 10:13:58 +0100

    update: refactor and add sass package

--- src/Console/Commands/AdminUIInstall.php
+++ src/Console/Commands/AdminUIInstall.php
@@ -12,6 +12,13 @@ class AdminUIInstall extends Command
      * @var string
      */
     protected $signature = 'admin-ui:install';
+    
+    /**
+     * Dependencies required to run a development enviroment
+     *
+     * @var array
+     */
+    private $devDependencies = ['vue-loader', 'sass-loader', 'sass', 'resolve-url-loader'];
 
     /**
      * The console command description.
@@ -72,11 +79,11 @@ class AdminUIInstall extends Command
         $packageJsonContent = json_decode($packageJson, JSON_OBJECT_AS_ARRAY);
         $packageJsonContent['devDependencies']['craftable'] = '^2.1.3';
 
-        $packageJsonContent['devDependencies']['vue-loader'] = '^15.9.6';
-        $packageJsonContent['devDependencies']['sass-loader'] = '^8.0.2';
-        $packageJsonContent['devDependencies']['resolve-url-loader'] = '^3.1.0';
-
         $files->put($packageJsonFile, json_encode($packageJsonContent, JSON_PRETTY_PRINT));
         $this->info('package.json changed');
+
+        $this->call('npm install', [
+            '--save-dev', join(' ', $devDependencies)
+        ]);
     }
 }
